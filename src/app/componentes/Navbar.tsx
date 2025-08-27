b2653f6ca4d089bd5d0173d4bc1a6a4316f239d6
kapilsingh09 <karan404katyura@gmail.com>
2025-08-27 23:59:26 +0530

    inital commit

---
+++ src/app/componentes/Navbar.tsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
