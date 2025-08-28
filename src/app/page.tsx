4439594187e1ef279af939df205082f596093049
kapilsingh09 <karan404katyura@gmail.com>
2025-08-28 15:50:21 +0530

    making components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { main } from "motion/react-client";
 import HeroSection from "@/app/componentes/HeroSection"
+import FeaturedSection from "./componentes/FeaturedSection";
 export default function Home() {
   return (
     <main className="bg-black/[0.96] min-h-screen antialiased bg-grid-white/[0.02]"  >
 
       <HeroSection />
+      <FeaturedSection />
     </main>
   );
 }
